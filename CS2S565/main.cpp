69366ac6402be9c8217ce43d62caa595146cb376
sebha01 <30077548@students.southwales.ac.uk>
2024-10-23 18:20:52 +0100

    Background 1 fills screen and faces right way now

--- CS2S565/main.cpp
+++ CS2S565/main.cpp
@@ -160,18 +160,17 @@ void drawBackGround(void)
 	glEnable(GL_TEXTURE_2D);
 	glBegin(GL_TRIANGLE_STRIP);
 
-	glTexCoord2f(0.0f, 0.0f);
-	glVertex2f(0.0f, 0.0f);
-
 	glTexCoord2f(0.0f, 1.0f);
-	glVertex2f(0.0f, 1.0f);
+	glVertex2f(-1.0f, -1.0f);
 
-	glTexCoord2f(1.0f, 0.0f);
-	glVertex2f(1.0f, 0.0f);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2f(-1.0f, 1.0f);
 
 	glTexCoord2f(1.0f, 1.0f);
-	glVertex2f(1.0f, 1.0f);
+	glVertex2f(1.0f, -1.0f);
 
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2f(1.0f, 1.0f);
 	
 
 	glEnd();
